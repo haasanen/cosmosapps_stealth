4b043f71dfec1b0e8459c0adec8fd61cdfea7cc0
haasanen <haasanen@users.noreply.github.com>
2026-09-18 16:00:28 +0000

    Bump to 2.5.90 (vc190)

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -7,6 +7,6 @@ object Config {
     const val compileSdk = 34
     const val targetSdk = 34
 
-    const val versionCode = 189
-    const val versionName = "2.5.89"
+    const val versionCode = 190
+    const val versionName = "2.5.90"
 }
